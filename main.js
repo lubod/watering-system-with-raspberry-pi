53955eda54622b426c1c6759b248dbf6965535a4
lubo <lubo.drobny@gmail.com>
2020-03-31 15:58:01 +0200

    Sync with prod

--- main.js
+++ main.js
@@ -120,7 +120,7 @@ app.get('/allCycle', function (req, res) {
         });
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(80, function () {
 
   var host = server.address().address
   var port = server.address().port
